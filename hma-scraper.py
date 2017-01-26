095ee1e5ae09833d01310476a3c166bdd3cb41fb
lsankar4033 <lsankar4033@gmail.com>
2017-01-26 17:17:46 +0000

    Fix indentation, var names

--- hma-scraper.py
+++ hma-scraper.py
@@ -11,35 +11,35 @@ def scrape_hma(uri):
     for line in r.text.splitlines():
         class_name = re.search(r'\.([a-zA-Z0-9_\-]{4})\{display:none\}', line)
         if class_name is not None:
-           bad_class += class_name.group(1)+'|'
+            bad_class += class_name.group(1)+'|'
 
     bad_class = bad_class.rstrip('|')
     bad_class += ')'
 
-    to_remove = '(<span class\="'+ bad_class + '">[0-9]{1,3}</span>|<span style=\"display:(none|inline)\">[0-9]{1,3}</span>|<div style="display:none">[0-9]{1,3}</div>|<span class="[a-zA-Z0-9_\-]{1,4}">|</?span>|<span style="display: inline">)'
+    to_remove = '(<span class\="' + bad_class + '">[0-9]{1,3}</span>|<span style=\"display:(none|inline)\">[0-9]{1,3}</span>|<div style="display:none">[0-9]{1,3}</div>|<span class="[a-zA-Z0-9_\-]{1,4}">|</?span>|<span style="display: inline">)'
 
     junk = re.compile(to_remove, flags=re.M)
     junk = junk.sub('', r.text)
     junk = junk.replace("\n", "")
 
     proxy_src = re.findall('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\s*</td>\s*<td>\s*([0-9]{2,6}).{100,1200}(socks4/5|HTTPS?)', junk)
-    
-    list = ''
+
+    proxies_str = ''
     for src in proxy_src:
         if src[2] == 'socks4/5':
             proto = 'socks5h'
         else:
             proto = src[2].lower()
         if src:
-            list += proto + '://' +src[0] + ':' + src[1] + '\n'
-    return(list)
+            proxies_str += proto + '://' +src[0] + ':' + src[1] + '\n'
+    return(proxies_str)
 
 if __name__ == "__main__":
     error = 'Input the number of pages to scrape. Ex:\npython hma-scraper.py 30'
     try:
         if sys.argv[1].isdigit() == True:
-            input = int(sys.argv[1])
-            for i in range(1,input):
+            num_pages = int(sys.argv[1])
+            for i in range(1, num_pages):
                 print(scrape_hma(str(i)), end = '')
         else:
             print(error)
